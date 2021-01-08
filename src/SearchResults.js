78606dc5e11c0e4588cc6c1163281eb125cfbeb2
esin87 <esin.saribudak@gmail.com>
2021-01-08 16:33:06 -0600

    Add form label for a11y and refactor SearchResults as functional component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,39 +2,43 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Gallery from './Gallery';
 
-class SearchResults extends React.Component {
-	render() {
-		if (!this.props.images) {
-			return <div>Loading...</div>;
-		}
-		return (
-			<Container style={{ marginTop: '1em' }}>
-				{this.props.setSearch && !this.props.error && (
-					<>
-						<p>
-							Showing results for{' '}
-							<span style={{ fontStyle: 'italic' }}>
-								{this.props.lastSearch}:
-							</span>{' '}
-						</p>
-						<Gallery
-							images={this.props.images}
-							getGalleryImages={this.props.getSearchImages}
-							searchOptions={this.props.searchOptions}
-							getMoreGalleryImages={this.props.getMoreSearchImages}
-						/>
-					</>
-				)}
-				{this.props.setSearch && this.props.error && (
-					<p>
-						No results found for{' '}
-						<span style={{ fontStyle: 'italic' }}>{this.props.lastSearch}</span>
-						. Please try another search
-					</p>
-				)}
-			</Container>
-		);
+const SearchResults = ({
+	images,
+	setSearch,
+	error,
+	lastSearch,
+	getSearchImages,
+	searchOptions,
+	getMoreSearchImages,
+}) => {
+	if (!images) {
+		return <div>Loading...</div>;
 	}
-}
+	return (
+		<Container style={{ marginTop: '1em' }}>
+			{setSearch && !error && (
+				<>
+					<p>
+						Showing results for{' '}
+						<span style={{ fontStyle: 'italic' }}>{lastSearch}:</span>{' '}
+					</p>
+					<Gallery
+						images={images}
+						getGalleryImages={getSearchImages}
+						searchOptions={searchOptions}
+						getMoreGalleryImages={getMoreSearchImages}
+					/>
+				</>
+			)}
+			{setSearch && error && (
+				<p>
+					No results found for{' '}
+					<span style={{ fontStyle: 'italic' }}>{lastSearch}</span>. Please try
+					another search
+				</p>
+			)}
+		</Container>
+	);
+};
 
 export default SearchResults;
