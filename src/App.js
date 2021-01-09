cac4e9fc37e6e3b63ad5f924ea676027d9870e67
esin87 <esin.saribudak@gmail.com>
2021-01-09 17:53:33 -0600

    Delete unused method

--- src/App.js
+++ src/App.js
@@ -105,10 +105,6 @@ class App extends React.Component {
 		this.setState({ searchString: event.target.value });
 	};
 
-	clearSearch = () => {
-		this.setState({ searchString: '', searchImages: '', setSearch: false });
-	};
-
 	render() {
 		return (
 			<Container>
@@ -149,7 +145,6 @@ class App extends React.Component {
 										setSearch={this.state.setSearch}
 										error={this.state.error}
 										searchImages={this.state.searchImages}
-										clearSearch={this.clearSearch}
 									/>
 								)}
 							/>
