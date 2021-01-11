5a2d65620dfecc620f426b43ae1828c2697b9f7b
esin87 <esin.saribudak@gmail.com>
2021-01-10 23:43:08 -0600

    Fix carousel slide flicker

--- src/CarouselContainer.js
+++ src/CarouselContainer.js
@@ -1,7 +1,8 @@
 //////CarouselContainer.js
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { CSSTransitionGroup } from 'react-transition-group';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const CarouselContainer = ({ data }) => {
 	return (
@@ -11,28 +12,45 @@ const CarouselContainer = ({ data }) => {
 			transitionAppearTimeout={500}
 			transitionEnter={false}
 			transitionLeave={false}>
-			<Carousel style={{ minHeight: '90vh' }}>
-				{data.map((item) => {
-					return (
-						<Carousel.Item key={item.id} style={{ maxHeight: '90vh' }}>
-							<img
-								className='d-block w-100'
-								style={{
-									height: '90vh',
-									width: '100%',
-									objectFit: 'cover',
-									overflow: 'hidden',
-								}}
-								src={item.webImage.url}
-								alt={item.title}
-							/>
-							<Carousel.Caption>
-								<p>{item.longTitle}</p>
-							</Carousel.Caption>
-						</Carousel.Item>
-					);
-				})}
-			</Carousel>
+			<Container>
+				<Row>
+					<Col>
+						<Carousel
+							style={{
+								minHeight: '90vh',
+								width: '100%',
+								backgroundColor: 'black',
+							}}
+							interval={2000}>
+							{data.map((item) => {
+								return (
+									<Carousel.Item
+										key={item.id}
+										style={{
+											maxHeight: '90vh',
+											width: '100%',
+										}}>
+										<img
+											className='d-block w-100'
+											style={{
+												height: '90vh',
+												width: '100%',
+												objectFit: 'cover',
+												overflow: 'hidden',
+											}}
+											src={item.webImage.url}
+											alt={item.title}
+										/>
+										<Carousel.Caption>
+											<p>{item.longTitle}</p>
+										</Carousel.Caption>
+									</Carousel.Item>
+								);
+							})}
+						</Carousel>
+					</Col>
+				</Row>
+			</Container>
 		</CSSTransitionGroup>
 	);
 };
