9369d7f2d31c3290713dd67eea7c253fc1deceaa
esin87 <esin.saribudak@gmail.com>
2021-01-15 20:48:49 -0600

    Refactor Gallery component using Hooks

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,5 +1,5 @@
 ///////Gallery.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -10,156 +10,151 @@ import Container from 'react-bootstrap/Container';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Detail from '../Detail/Detail.js';
-class Gallery extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			show: false,
-			activeItem: '',
-			error: false,
-		};
-	}
 
-	componentDidMount() {
-		if (!this.props.images.length) this.props.getGalleryImages();
-	}
+const Gallery = ({
+	images,
+	getGalleryImages,
+	searchOptions,
+	getMoreGalleryImages,
+	darkScheme,
+}) => {
+	const [show, setShow] = useState(false);
+	const [activeItem, setActiveItem] = useState('');
+	const [error, setError] = useState(false);
 
-	handleShow = () => {
-		this.setState({ show: true });
+	useEffect(() => {
+		if (!images.length) getGalleryImages();
+		//eslint-disable-next-line
+	}, []);
+
+	const handleShow = () => {
+		setShow(true);
 	};
 
-	handleClose = () => {
-		this.setState({ show: false, error: false, activeItem: '' });
+	const handleClose = () => {
+		setShow(false);
+		setError(false);
+		setActiveItem('');
 	};
 
-	handleError = () => {
-		this.setState({ error: true });
+	const handleError = () => {
+		setError(true);
 	};
 
-	getDetail = (itemId) => {
-		fetch(
-			`${this.props.searchOptions.url}/collection/${itemId}?key=${this.props.searchOptions.key}`
-		)
+	const getDetail = (itemId) => {
+		fetch(`${searchOptions.url}/collection/${itemId}?key=${searchOptions.key}`)
 			.then((res) => res.json())
 			.then((res) => {
-				this.setState({ activeItem: res });
+				setActiveItem(res);
 			})
-			.then((res) => this.handleShow())
+			.then((res) => handleShow())
 			.catch((err) => {
-				this.handleError();
-				this.handleShow();
+				handleError();
+				handleShow();
 			});
 	};
 
-	render() {
-		if (this.props.images) {
-			return (
-				<InfiniteScroll
-					dataLength={this.props.images.length}
-					next={this.props.getMoreGalleryImages}
-					hasMore={true}
-					loader={
-						<Container
-							className='d-flex  justify-content-center align-items-center align-content-center'
-							style={{ minHeight: '10vh' }}>
-							<span
-								style={{
-									paddingRight: '1em',
-									color: this.props.darkScheme === 'on' ? 'white' : '',
-								}}>
-								Loading more results ...
-							</span>
-						</Container>
-					}>
-					<Container>
-						<CardGroup>
-							<Row>
-								{this.props.images.map((object) => {
-									return (
-										<Col
-											key={object.id}
-											xs={12}
-											s={12}
-											md={6}
-											lg={4}
-											xl={3}
-											style={{ marginTop: '1em' }}>
-											<Card
-												style={{
-													border:
-														this.props.darkScheme === 'on'
-															? '1px solid #6c757'
-															: '',
-												}}>
-												{object.webImage && (
-													<Card.Img
-														variant='top'
-														src={object.webImage ? object.webImage.url : ''}
-														alt={object.title}
-													/>
-												)}
-												<Card.Body
-													style={{
-														backgroundColor:
-															this.props.darkScheme === 'on' ? '#292b2c' : '',
-													}}>
-													{object.webImage ? (
-														''
-													) : (
-														<Card.Title>No Image Available</Card.Title>
-													)}
-													<Card.Text className='text-muted'>
-														{object.longTitle}
-													</Card.Text>
-													<Button
-														onClick={() => this.getDetail(object.objectNumber)}
-														variant={
-															this.props.darkScheme === 'on'
-																? 'dark'
-																: 'outline-dark'
-														}>
-														Details
-													</Button>
-												</Card.Body>
-											</Card>
-										</Col>
-									);
-								})}
-								{(this.state.activeItem || this.state.error) && (
-									<Detail
-										objectDetail={this.state.activeItem}
-										show={this.state.show}
-										handleClose={this.handleClose}
-										error={this.state.error}
-										darkScheme={this.props.darkScheme}
-									/>
-								)}
-							</Row>
-						</CardGroup>
+	if (images) {
+		return (
+			<InfiniteScroll
+				dataLength={images.length}
+				next={getMoreGalleryImages}
+				hasMore={true}
+				loader={
+					<Container
+						className='d-flex  justify-content-center align-items-center align-content-center'
+						style={{ minHeight: '10vh' }}>
+						<span
+							style={{
+								paddingRight: '1em',
+								color: darkScheme === 'on' ? 'white' : '',
+							}}>
+							Loading more results ...
+						</span>
 					</Container>
-				</InfiniteScroll>
-			);
-		} else {
-			return (
-				<Container
-					className='d-flex  justify-content-center align-items-center align-content-center'
-					style={{ minHeight: '90vh' }}>
-					<span
-						style={{
-							paddingRight: '1em',
-							color: this.props.darkScheme === 'on' ? 'white' : '',
-						}}>
-						Loading results{'  '}
-					</span>
-					<Spinner
-						animation='border'
-						variant={this.props.darkScheme === 'on' ? 'light' : 'dark'}
-						size='sm'
-					/>
+				}>
+				<Container>
+					<CardGroup>
+						<Row>
+							{images.map((object) => {
+								return (
+									<Col
+										key={object.id}
+										xs={12}
+										s={12}
+										md={6}
+										lg={4}
+										xl={3}
+										style={{ marginTop: '1em' }}>
+										<Card
+											style={{
+												border: darkScheme === 'on' ? '1px solid #6c757' : '',
+											}}>
+											{object.webImage && (
+												<Card.Img
+													variant='top'
+													src={object.webImage ? object.webImage.url : ''}
+													alt={object.title}
+												/>
+											)}
+											<Card.Body
+												style={{
+													backgroundColor: darkScheme === 'on' ? '#292b2c' : '',
+												}}>
+												{object.webImage ? (
+													''
+												) : (
+													<Card.Title>No Image Available</Card.Title>
+												)}
+												<Card.Text className='text-muted'>
+													{object.longTitle}
+												</Card.Text>
+												<Button
+													onClick={() => getDetail(object.objectNumber)}
+													variant={
+														darkScheme === 'on' ? 'dark' : 'outline-dark'
+													}>
+													Details
+												</Button>
+											</Card.Body>
+										</Card>
+									</Col>
+								);
+							})}
+							{(activeItem || error) && (
+								<Detail
+									objectDetail={activeItem}
+									show={show}
+									handleClose={handleClose}
+									error={error}
+									darkScheme={darkScheme}
+								/>
+							)}
+						</Row>
+					</CardGroup>
 				</Container>
-			);
-		}
+			</InfiniteScroll>
+		);
+	} else {
+		return (
+			<Container
+				className='d-flex  justify-content-center align-items-center align-content-center'
+				style={{ minHeight: '90vh' }}>
+				<span
+					style={{
+						paddingRight: '1em',
+						color: darkScheme === 'on' ? 'white' : '',
+					}}>
+					Loading results{'  '}
+				</span>
+				<Spinner
+					animation='border'
+					variant={darkScheme === 'on' ? 'light' : 'dark'}
+					size='sm'
+				/>
+			</Container>
+		);
 	}
-}
+};
 
 export default Gallery;
